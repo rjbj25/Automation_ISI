a54033d16a63fa88e9a8811666e374cb71e42c80
Ramiro Balmaceda <ramirobj.25@gmail.com>
2023-10-25 08:33:12 -0500

    update

--- Scripts/fill_crm/q10scraper.py
+++ Scripts/fill_crm/q10scraper.py
@@ -21,7 +21,7 @@ def main():
                     q10p.go_to_oportunities()
                     q10p.register_oportunitie(student.iloc[i])
                     q10p.register_oportunitie_detail(student.iloc[i])
-                    q10p.save_oportunitie()
+                    #q10p.save_oportunitie()
                     q10p._driver.quit()
                 except Exception as e:
                     logger.error(e)
